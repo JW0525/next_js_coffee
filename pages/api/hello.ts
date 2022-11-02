9fac8eef0fe2232c7ce7fb974e55155dd42c2350
JW0525 <lemuel0525@gmail.com>
2022-11-02 16:37:33 +0900

    feat: API 전송 기능 추가

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,12 +2,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  [key: string]: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
   res.status(200).json({ name: 'John Doe' })
+
+  return res.status(200)
+    .send({
+      test: 'This is test API'
+    })
 }
