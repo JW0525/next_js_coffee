d15515e5f81016419378e0450b753c594ef45ba4
JW0525 <lemuel0525@gmail.com>
2022-12-20 00:53:34 +0900

    feat: categryId 파일 connectDB 추가

--- pages/api/order/[categoryId].ts
+++ pages/api/order/[categoryId].ts
@@ -1,8 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { CategoryList } from '../database/model';
+import connectDB from "../database/connectDB";
 
 export default async function getIdData(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  connectDB().catch(error => console.error(error));
+
   const { categoryId } = req.query;
   const data = await CategoryList.find({}).limit(1);
 
@@ -10,7 +13,6 @@ export default async function getIdData(req: NextApiRequest, res: NextApiRespons
     case 'GET':
       try {
         const { categoryList } = data[0];
-        // const category = categoryList.find((value: any) => value.id.toString() === categoryId);
         res.status(200).json(categoryList);
       } catch (err) {
         res.status(400).json({ status: false });
