cdacb3fea6782d28d7201993dfe7d4e1cb272fb1
eymin1259 <eymin1259@gmail.com>
2023-03-06 22:17:11 +0900

    레이아웃 수정 : 직원모드 관리자모드 구분

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,11 +1,35 @@
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useMemo } from "react";
 import styled from "@emotion/styled";
-import Navigation from "@/components/layout/navigation";
+import TabBar from "@/components/layout/tabBar";
 import { useAuth } from "hooks/common/useAuth";
+import Header from "./header";
 
 interface ILayoutContainerProps {
-  isWide: boolean;
+  isManagerMode: boolean;
 }
+
+export const Layout: FC<{ children: ReactNode }> = ({ children }) => {
+  const { isLogin, userInfo } = useAuth();
+
+  const isManagerMode = useMemo(() => {
+    return isLogin && userInfo.isManager;
+  }, [isLogin, userInfo.isManager]);
+
+  const isEmployeeMode = useMemo(() => {
+    return isLogin && !userInfo.isManager;
+  }, [isLogin, userInfo.isManager]);
+
+  return (
+    <LayoutContainer>
+      {/* {isManagerMode && <SideBar/>} */}
+      <MainContainer isManagerMode={isManagerMode}>
+        {isLogin && <Header />}
+        {children}
+      </MainContainer>
+      {isEmployeeMode && <TabBar />}
+    </LayoutContainer>
+  );
+};
 
 const LayoutContainer = styled.div`
   display: flex;
@@ -15,59 +39,18 @@ const LayoutContainer = styled.div`
   height: 100vh;
   background-color: floralwhite;
 
-  main {
-    padding-bottom: 80px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: 100vh;
-  }
-
-  @media screen and (max-width: 480px) {
-    .navigation {
-      width: 100vw;
-    }
-    main {
-      width: 100vw;
-      background-color: white;
-    }
-  }
-
-  @media screen and (min-width: 480px) {
-    .navigation {
-      width: 480px;
-    }
-    main {
-      display: flex;
-      justify-content: center;
-      width: ${(props: ILayoutContainerProps) =>
-        props.isWide ? "80%" : "480px"};
-      background-color: white;
-    }
-  }
-
-  footer {
-    position: absolute;
-    bottom: 0;
-  }
-
-  // 컴포넌트 CSS
-  .page-container {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    max-height: 1536px;
-    overflow: auto;
+  .layout-tabBar {
+    width: 480px;
   }
 `;
 
-export const Layout: FC<{ children: ReactNode }> = ({ children }) => {
-  const { isLogin, userInfo } = useAuth();
-
-  return (
-    <LayoutContainer isWide={isLogin && userInfo.isManager}>
-      <main>{children}</main>
-      {isLogin && !userInfo.isManager && <Navigation />}
-    </LayoutContainer>
-  );
-};
+const MainContainer = styled.div`
+  padding-bottom: 80px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  height: 100vh;
+  width: ${(props: ILayoutContainerProps) =>
+    props.isManagerMode ? "80%" : "480px"};
+  background-color: white;
+`;
